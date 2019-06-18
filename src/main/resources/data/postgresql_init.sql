fe8680a2c00c1194cbd33e77d2982d7c34fb7399
libsgh <woiyyng@gmail.com>
2019-06-18 19:44:17 +0800

    update
    Signed-off-by: libsgh <woiyyng@gmail.com>

--- src/main/resources/data/postgresql_init.sql
+++ src/main/resources/data/postgresql_init.sql
@@ -1,5 +1,4 @@
-CREATE TABLE IF NOT EXISTS "public"."config" ("key" text COLLATE "pg_catalog"."default" NOT NULL,"value" text COLLATE "pg_catalog"."default");
-ALTER TABLE "public"."config" ADD CONSTRAINT "config_pkey" PRIMARY KEY ("key");
+CREATE TABLE IF NOT EXISTS "public"."config" ("key" text COLLATE "pg_catalog"."default" NOT NULL,"value" text COLLATE "pg_catalog"."default",CONSTRAINT "config_pkey" PRIMARY KEY ("key"));
 INSERT INTO "public"."config" VALUES ('clientId', '');
 INSERT INTO "public"."config" VALUES ('clientSecret', '');
 INSERT INTO "public"."config" VALUES ('redirectUri', 'http://localhost:8080/authRedirect');
